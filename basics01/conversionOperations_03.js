11d40bd72fcfaec65474ff5c09ee4fc1647c9703
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-02-02 12:40:05 +0530

    created datatype conversion operations

---
+++ basics01/conversionOperations_03.js
@@ -0,0 +1,17 @@
+let score= "55"
+ console.log(typeof score);
+ console.log( typeof score);
+
+
+ let valueInNumber=Number(score)
+ console.log( typeof valueInNumber);
+     
+ let isloggedIn = 1
+let booleanIsloggedIn = Boolean(isloggedIn)
+ console.log(booleanIsloggedIn);
+
+ let someNumber = 23
+
+ let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
