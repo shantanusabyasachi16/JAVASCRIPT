bb7f6e4580ff4dfc2cb40b8723cd6970ea646521
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-02-11 15:37:41 +0530

    Add files via upload

---
+++ basics01/strings_05.js
@@ -0,0 +1,33 @@
+const name = "dugu"
+ const repocount = 30
+//   console.log( name+repocount+"value");
+// concatenation means to link together. we often use it programming  to concatenation to combine strings , arrays,and other data structures
+
+console.log(`Hello my name ${name} and my repocount is ${repocount}`);
+
+//It is knows as strings interpolation. It is concept to embed variables or expressions within a strings.
+
+
+const gameName = new String('duguuuuu ')
+console.log(gameName);
+
+// The advantage of declaring a string ina objective way is
+//there are some predefined prototype functions or method are available.
+//ex-
+
+
+ console.log(gameName[1]);//u
+ console.log(gameName.length);//8
+  console.log(gameName.toUpperCase()); //DUGUUUUU
+
+  //it doesn't change the original value
+
+  console.log(gameName.charAt(4)); //u
+  console.log( gameName.indexOf('g'));//2
+
+  
+
+
+
+
+ 
