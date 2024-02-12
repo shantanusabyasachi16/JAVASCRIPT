b1f0c263e44ba1f678d6c337e1309571e6fcc4d7
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-02-12 12:30:58 +0530

    Update strings_05.js

--- basics01/strings_05.js
+++ basics01/strings_05.js
@@ -8,7 +8,7 @@ console.log(`Hello my name ${name} and my repocount is ${repocount}`);
 //It is knows as strings interpolation. It is concept to embed variables or expressions within a strings.
 
 
-const gameName = new String('duguuuuu ')
+const gameName = new String('shantan-u- com')
 console.log(gameName);
 
 // The advantage of declaring a string ina objective way is
@@ -16,16 +16,39 @@ console.log(gameName);
 //ex-
 
 
- console.log(gameName[1]);//u
- console.log(gameName.length);//8
-  console.log(gameName.toUpperCase()); //DUGUUUUU
+ console.log(gameName[1]);//h
+ console.log(gameName.length);//14
+  console.log(gameName.toUpperCase()); //SHANTAN-U-COM
 
   //it doesn't change the original value
 
-  console.log(gameName.charAt(4)); //u
-  console.log( gameName.indexOf('g'));//2
+  console.log(gameName.charAt(4)); //T
+  console.log( gameName.indexOf('h'));//1
 
   
+ 
+ const newstring = gameName.substring(0,5)
+  console.log(newstring);//shant
+  //it can't take negative values.
+    
+  const anotherstring =gameName.slice(-9,3)
+  console.log(anotherstring);//sha
+
+   const newstring0ne = "   my name   "
+   console.log(newstring0ne);
+   console.log(newstring0ne.trim());//   my name
+   my name
+   //it removes the white space characters.
+
+   const url = "https://dugu.com/duguguu%20off"//https://dugu.com/duguguu-off
+   console.log(url.replace('%20','-'));
+
+   console.log(url.includes('dugu'));//true
+   console.log(url.includes('myname'));//false
+
+   console.log( gameName.split('-'));//[ 'shantan', 'u', ' com' ]
+//It splits the string on the basis of a particular parameter here it is "-"
+
 
 
 
