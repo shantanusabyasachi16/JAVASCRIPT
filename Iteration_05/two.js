826a2114cea20a72ade598d8bef4c7b8da212f0c
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-04-01 22:23:45 +0530

    while _do while loop

--- Iteration_05/two.js
+++ Iteration_05/two.js
@@ -2,7 +2,7 @@
 
 let index = 6
 while (index<=20) {
-    console.log(`value of index:${index}`);
+    //console.log(`value of index:${index}`);
     index= index + 3
     
 }
@@ -18,7 +18,7 @@ value of index:18*/
 let mynewarray= ["kkr","csk","rcb"]
 let index1 = 0
 while (index1<mynewarray.length) {
-    console.log(`value is :${mynewarray[index1]}`);
+    //console.log(`value is :${mynewarray[index1]}`);
     index1 = index1 + 1
     
 }
@@ -28,3 +28,33 @@ value is :csk
 value is :rcb*/
 
 // do while loop
+
+let score = 1
+
+do {
+    
+    console.log(`score is:${score}`);
+    score ++
+} while (score<=10);
+/*
+score is:1
+score is:2
+score is:3
+score is:4
+score is:5
+score is:6
+score is:7
+score is:8
+score is:9
+score is:10*/
+
+//what happens if the score value is more  than the givemn condition
+let score1 = 11
+
+do {
+    
+    console.log(`score is:${score1}`);
+    score1 ++
+} while (score1<=10);
+//score is:11
+//it will execute beacause in do while loop the condition is checked after the work
