2f0c69999a596b0095d13de3611c64440f776663
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-04-04 16:42:19 +0530

    learning forEachloops

---
+++ forEachLoop.js
@@ -0,0 +1,30 @@
+const coding=["js","java","c++","c"]
+ //for (const i of coding) {
+   // console.log(i);
+    
+ //}
+  // for (const key in coding) {
+    
+    //console.log(coding[key]);
+    //}
+
+    /// for each loop
+
+    coding.forEach(function (i) {
+        console.log(i);
+        
+    })
+   
+/*js
+java
+c++
+c*/ 
+
+// in arrow function ( () =>{} )
+coding.forEach((i)=>{
+    console.log(i);
+})
+ /*js
+java
+c++
+c*/ 
