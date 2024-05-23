791b0cf49893a52273ece3258b52ff93a6177b57
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-05-23 12:05:47 +0530

    learning getter and setter

---
+++ advance_10/getterAndSetters.js
@@ -0,0 +1,13 @@
+// Getters
+
+//A getter is a method that gets called when you try to access a property of an object.
+
+//It allows you to execute some code before returning the property value.
+
+// Getters are defined using the get keyword followed by the property name.
+
+// Setter
+
+// A setter is a method that gets called when you try to assign a value to a property of an object.
+
+// It allows you to execute some code before setting the property value. Setters are defined using the set keyword followed by the property name.
